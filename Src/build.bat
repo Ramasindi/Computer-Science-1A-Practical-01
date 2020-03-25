ebbc15a22274413d658b0c6553afd8fb86a0ddc5
Thalukanyo <thaluramasindi@gmail.com>
2020-03-25 16:37:06 +0200

    Project files

---
+++ Src/build.bat
@@ -0,0 +1,6 @@
+@echo off
+del *.o
+del *.exe
+g++ main.cpp -o 123456789
+123456789
+pause
---
+++ Src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << " Student no:123456789 \n SR no: 123" << endl;
+    return 0;
+}
